053bc2ba99d362f5175811c051d92b5fe55ca6a2
Madhu Kushwaha <madhu.kushwaha9005@gmail.com>
2026-04-24 23:37:17 +0530

    Loops in Java.

---
+++ src/Loops.java
@@ -0,0 +1,62 @@
+import java.sql.SQLOutput;
+
+public class Loops {
+
+    static void main() {
+
+        //First Loop
+//        for(int i=1; i<=5;i++){
+//            System.out.println("Value of i is:"+  i);
+//        }
+      //Second loop
+//        for(int i=1; i<=10;i=i+2){
+//            System.out.println( i);
+//        }
+        //Third loop
+//        for(int i=2; i<=10;i=i+2){
+//            System.out.println( i);
+//        }
+       // Fourth Loop
+//       for(int i=1;i<=4; i++) {
+//           for(int j=1; j<=4; j++){
+//               System.out.print(" * ");
+//           }
+//           System.out.println();
+//       }
+
+        //Fifth Loop
+//       for(int i=1;i<=3; i++) {
+//           for(int j=1; j<=3; j++){
+//               System.out.println(" i = " + i + " j=" + j);
+//           }
+//       }
+        //Sixth Loop
+//        for(int i=1;i<=10; i++){
+//            if(i ==5 ){
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+        //Seventh Loop
+//        for(int i=1;i<=10; i++){
+//            if(i ==5 ){
+//                continue;  // Updation yhi hoga
+//            }
+//            System.out.println(i);
+//        }
+// Eighth Loop
+      int i =1;
+      while(i<=5){
+          System.out.println(i);
+          i++;
+      }
+        //Ninth Loop
+
+
+    }
+
+}
+
+/// Loop concept
+//outer loop ke liye inner loop pura chlega
