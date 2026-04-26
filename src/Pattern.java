22f4cfec46f29450ccc55024734cedbacba01649
Madhu Kushwaha <madhu.kushwaha9005@gmail.com>
2026-04-26 18:13:27 +0530

    patterns  in Java.

--- src/Pattern.java
+++ src/Pattern.java
@@ -2,23 +2,101 @@ public class Pattern {
 
     static void main() {
 
-
+    //pattern 1
 //       for(int i=1;i<=4; i++) {
 //           for(int j=1; j<=4; j++){
 //               System.out.print(" * ");
 //           }
 //           System.out.println();
 //       }
+     //pattern 2
+//        int n =5;
+//
+//        for( int row =1; row<=n; row++){
+//            for(int col=1; col<=row; col++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
 
-        int n =5;
+        //pattern 3
+//        int n =5;
+//
+//        for( int row =1; row<=n; row++){
+//            for(int col=1; col <= n-row; col++){
+//                System.out.print(" * ");
+//            }
+//            System.out.println();
+//        }
 
-        for( int row =1; row<=n; row++){
-            for(int col=1; col<=row; col++){
-                System.out.print("* ");
+        //pattern 4
+//        int n =5;
+//
+//        for( int row =1; row<=n; row++){
+//
+//            //spaces
+//            for(int col=1; col<=n-row; col++){
+//                System.out.print(" ");
+//            }
+//
+//            //stars
+//            for(int col=1; col<=n; col++){
+//                System.out.print(" * ");
+//            }
+//            System.out.println();
+//        }
+
+        //pattern 5
+//        int n =5;
+//
+//        for( int row =1; row<=n; row++){
+//
+//            //spaces
+//            for(int col=1; col<=n-row; col++){
+//                System.out.print(" ");
+//            }
+//
+//            //stars
+//            for(int col=1; col<=2*row-1; col++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+       //pattern 6
+
+        int n = 4;
+
+        for(int row =1; row<=n ; row++){
+            //spaces
+            for(int col=1;col<=row-1; col++){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=2*n-2*row+1; col++){
+                System.out.print("*");
             }
             System.out.println();
         }
+//pattern 9
 
+//        int  n=4;
+//        for(int row =1; row<=n ; row++){
+//            //spaces
+//           for(int col =1;col<=6;col++){
+//               if(row==1 || row==n){
+//                   System.out.print("* ");
+//               }else{
+//                   if(col ==1 || col == 4){
+//                       System.out.print("*");
+//                   }
+//                   else {
+//                       System.out.print(" ");
+//                   }
+//               }
+//           }
+//            System.out.println(" ");
+//        }
 
     }
 }
