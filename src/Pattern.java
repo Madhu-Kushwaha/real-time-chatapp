9e2b1864b244f8a6c7ec00e794271baac3b5ce8b
Madhu Kushwaha <madhu.kushwaha9005@gmail.com>
2026-04-29 18:17:45 +0530

    Hollow patterns  in Java.

--- src/Pattern.java
+++ src/Pattern.java
@@ -1,15 +1,17 @@
+import java.sql.SQLOutput;
+
 public class Pattern {
 
     static void main() {
 
-    //pattern 1
+        //pattern 1
 //       for(int i=1;i<=4; i++) {
 //           for(int j=1; j<=4; j++){
 //               System.out.print(" * ");
 //           }
 //           System.out.println();
 //       }
-     //pattern 2
+        //pattern 2
 //        int n =5;
 //
 //        for( int row =1; row<=n; row++){
@@ -64,7 +66,7 @@ public class Pattern {
 //        }
 
 
-       //pattern 6
+        //pattern 6
 
 ////        int n = 4;
 //
@@ -121,58 +123,92 @@ public class Pattern {
 
 
         //pattern 11
-        int n =4;
+//        int n =4;
+//
+//   for( int row =1; row<=n; row++){
+//
+//            //spaces
+//            for(int col=1; col<=n-row; col++){
+//                System.out.print("  ");
+//            }
+//
+//            //stars
+//                if(row==1){
+//                    for(int col=1;col<=2*row-1;col++) {
+//                        System.out.print("* ");
+//                    }
+//               }
+//                else {
+//                    System.out.print("* ");
+//                    for(int col=1; col<=2*row-3;col++){
+//                        System.out.print("  ");
+//                    }
+//                    System.out.print("* ");
+//                }
+//            System.out.println();
+//            }
+//
+//         //part 2
+//
+//        for(int row=1; row<=(n-1);row++) {
+//
+//            for (int col = 1; col <=row; col++) {
+//                System.out.print("  ");
+//            }
+//
+//            if(row==(n-1)){
+//                System.out.print("* ");
+//            }
+//            else{
+//                //1*
+//                System.out.print("* ");
+//                for(int col=1;col<=2*(n-row)-3;col++){
+//                    System.out.print("  ");
+//                }
+//                System.out.print("* ");
+//            }
+//
+//        }
 
-   for( int row =1; row<=n; row++){
+        //pattern12
+//        int n = 5;
+//        for (int row = 1; row <= n; row++) {
+//            for(int col = 1; col <= row; col++){
+//                System.out.print(col+ " ");
+//            }
+//            System.out.println();
+//        }
+///  Not understood pattern
+//        int n = 5;
+//        for(int row =1; row<=n ; row++){
+//           for(int col=1; col<=row;col++){
+//               int a = col;
+//               int b = ('A' - 1);
+//               int ans = a+b;
+//               char finalAns = (char)ans;
+//               System.out.print(finalAns + " ");
+//           }
+//            System.out.println();
+//        }
 
-            //spaces
-            for(int col=1; col<=n-row; col++){
-                System.out.print("  ");
-            }
 
-            //stars
-                if(row==1){
-                    for(int col=1;col<=2*row-1;col++) {
-                        System.out.print("* ");
-                    }
-               }
-                else {
-                    System.out.print("* ");
-                    for(int col=1; col<=2*row-3;col++){
-                        System.out.print("  ");
-                    }
-                    System.out.print("* ");
-                }
+// pattern 13
+        int n = 5;
+        for(int row =1; row<=n ; row++){
+           for(int col=1; col<=row;col++){
+               int a = n-col;
+               int b = 'A';
+               int ans = a+b;
+               char finalAns = (char)ans;
+               System.out.print(finalAns + " ");
+           }
             System.out.println();
-            }
-
-         //part 2
-
-        for(int row=1; row<=(n-1);row++) {
-
-            for (int col = 1; col <=row; col++) {
-                System.out.print("  ");
-            }
-
-            if(row==(n-1)){
-                System.out.print("* ");
-            }
-            else{
-                //1*
-                System.out.print("* ");
-                for(int col=1;col<=2*(n-row)-3;col++){
-                    System.out.print("  ");
-                }
-                System.out.print("* ");
-            }
-
         }
 
-        }
+        //pattern14
 
 
-
-
+    }
 }
 
 
